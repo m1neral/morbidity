ae3c0fbad9b747f2cb8b471ae320ac5323916d09
m1neral <m1neral@yandex.ru>
2015-02-15 20:48:43 +0300

    Refresh DB by migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 1) do
 
   create_table "diseases", force: :cascade do |t|
     t.string "name", limit: 255, null: false
