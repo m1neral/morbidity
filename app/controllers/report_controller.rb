7e9227a72d3a9704d4271c65dcdffc11ce7afd87
m1neral <m1neral@yandex.ru>
2015-02-24 22:16:38 +0300

    Little refactored

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,26 +1,25 @@
 class ReportController < ApplicationController
-  include ActionView::Helpers::TextHelper
-  before_action :get_fos, :get_first_month, :get_last_month
+  before_action :get_fos, :get_date_restriction
 
   def index
   end
 
   def handling
     @report = 'There should be a report'
+    #date = params[:date].delete '-'
+    #fo = params[:territory]
+    #render text: fo
     render :index
   end
 
-  private
+private
 
   def get_fos
-    @fos = Territory.find_fos
+    @fos = Territory.get_fos
   end
 
-  def get_first_month
+  def get_date_restriction
     @first_month = Statistic.find_first_month.to_s.insert(4, '-')[0,7]
-  end
-
-  def get_last_month
     @last_month = Statistic.find_last_month.to_s.insert(4, '-')[0,7]
   end
 
--- app/models/territory.rb
+++ app/models/territory.rb
@@ -1,10 +1,10 @@
 class Territory < ActiveRecord::Base
-  @@fo = 1 # Федеральный округ - 1
+  FO_ID = 1 # Федеральный округ - 1
 
   has_many :hospitals, foreign_key: 'terr_id'
   belongs_to :parent, class_name: 'Territory', foreign_key: 'parent_id'
 
-  def self.find_fos
-    where 'parent_id = ?', @@fo
+  def self.get_fos
+    where 'parent_id = ?', FO_ID
   end
 end
