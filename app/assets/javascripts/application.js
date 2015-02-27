b8d71590a41511769c238ef293401e8c0ba91fca
m1neral <m1neral@yandex.ru>
2015-02-27 21:19:10 +0300

    Added sortable table, other style changes. Second final version.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,4 +9,10 @@ $(document).ready(function(){
         minViewMode: "months",
         autoclose: true
     });
+    $('table').DataTable({
+        "iDisplayLength": 25,
+        "aaSorting": [],
+        "language": {
+        "url": "http://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Russian.json" }
+    });
 })
